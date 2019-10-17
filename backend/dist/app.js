29025e06785504ae01a4cc02f84bd51920927270
oscar <36045526+oscarblanq@users.noreply.github.com>
2019-10-17 05:16:01 +0200

    middleware implementado

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const config_1 = require("./config/config");
+const middleware_1 = __importDefault(require("./middleware"));
 const app = express_1.default();
 const config = new config_1.Config();
-// Configuración
+app.use(middleware_1.default);
 app.set('port', config.port);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
+import { handleBodyRequestParsing } from './middleware/common';
 import express, { Application } from 'express';
 import {Config} from './config/config';
+import middleware from './middleware';
 
 const app: Application = express();
 const config: Config = new Config();
 
-// Configuración
+app.use(middleware);
+
 app.set('port', config.port);
 
 export default app;
