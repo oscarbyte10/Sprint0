2e0834ffc09a775d1f81ffbaa77f98e5c51e991a
oscar <36045526+oscarblanq@users.noreply.github.com>
2019-10-17 04:37:50 +0200

    Estructura mejorada

---
+++ backend/dist/middleware/common.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
+exports.handleBodyRequestParsing = (router) => {
+    router.use(body_parser_1.default.urlencoded({ extended: true }));
+    router.use(body_parser_1.default.json());
+};
+//# sourceMappingURL=common.js.map
